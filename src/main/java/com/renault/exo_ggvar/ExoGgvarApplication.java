bbc2a1d9381972861305f03569773453aa34955f
abdelalim.hankafi <a.hankafi@externe.labelvie.ma>
2026-03-07 16:36:16 +0000

    Initial commit

---
+++ src/main/java/com/renault/exo_ggvar/ExoGgvarApplication.java
@@ -0,0 +1,13 @@
+package com.renault.exo_ggvar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoGgvarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoGgvarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renault/exo_ggvar/ExoGgvarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renault.exo_ggvar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoGgvarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
